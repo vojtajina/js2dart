7e90604def4ff4ccd8f643f7ea138345204458c6
Tobias Bosch <tbosch1009@gmail.com>
2014-09-18 15:01:02 -0700

    fix(gulp): simplify and add options to reload the compiler sources

--- gulp-js2dart.js
+++ gulp-js2dart.js
@@ -1,28 +1,26 @@
 var util = require('util');
 var through = require('through2');
-var JS2DartCompiler = require('./src/compiler');
 var HEADER = '// Compiled by js2dart\n' +
              '// Source file: %s\n\n';
-
-module.exports = function js2dart() {
-  var compiler = new JS2DartCompiler();
-
+module.exports = function js2dart(options) {
+  var compiler;
+  if (!options || !options.reload) {
+    createCompiler();
+  }
   return through.obj(function(file, _, done) {
-    try {
-      var compiled = compiler.compile(file.contents.toString(), file.path);
-      // TODO(vojta): Create new file object.
-      file.contents = new Buffer(util.format(HEADER, file.path) + compiled);
-      this.push(file);
-      done();
-    } catch (errors) {
-      errors.forEach(function(errorMsg) {
-        console.error(errorMsg);
-      });
-
-      // This is lame, plugin should not kill the VM.
-      // Unfortunately, Gulp sucks at dealing with errors.
-      // TODO(vojta): use some plugin that handles this nicely.
-      process.exit(1);
+    if (options && options.reload) {
+      createCompiler();
     }
+    var compiled = compiler.compile(file.contents.toString(), file.path);
+    // TODO(vojta): Create new file object.
+    file.contents = new Buffer(util.format(HEADER, file.path) + compiled);
+    this.push(file);
+    done();
   });
+
+  function createCompiler() {
+    var JS2DartCompiler = require('./src/compiler');
+    compiler = new JS2DartCompiler();
+  }
+
 };
