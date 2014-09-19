4e1ebfdefda333354bbda71e172daa5db4808616
Tobias Bosch <tbosch1009@gmail.com>
2014-09-19 13:54:03 -0700

    build: make grunt file reusable

--- gulpfile.js
+++ gulpfile.js
@@ -8,57 +8,88 @@ var clean = require('gulp-rimraf');
 var runSequence = require('run-sequence');
 var mergeStreams = require('event-stream').merge;
 
-var traceurJsOptions = {
-  annotations: true, // parse annotations
-  types: true, // parse types
-  script: false, // parse as a module
-  modules: 'register',
-  typeAssertions: false,
-  moduleName: true,
-  referrer: 'js2dart',
-  reload: true
-};
-
 var baseDir = __dirname;
-var srcFiles = baseDir + '/src/**/*.js';
-var specFiles = baseDir + '/spec/**/*.js';
-var fixtureFiles = baseDir + '/spec/**/*.dart';
-var buildDir = baseDir + '/build';
+var traceurDir = baseDir+'/../traceur';
+var traceurFiles = traceurDir+'/src/**/*.js';
 
-gulp.task('js2dart/clean', function() {
-  return gulp.src(buildDir, {read: false})
-      .pipe(clean());
-});
+install(gulp);
+module.exports.install = install;
 
-gulp.task('js2dart/build', function() {
-  return gulp
-    .src(srcFiles)
-    .pipe(traceur(traceurJsOptions))
-    .pipe(gulp.dest(buildDir+'/js2dart'))
-});
+function install(gulp) {
+  // -- traceur
+  gulp.task('traceur/clean', shell.task(['cd '+traceurDir+' && make clean']));
 
-gulp.task('js2dart/test/build', function() {
-  var transpileSrc =
-    gulp
-      .src(specFiles)
-      .pipe(js2dart())
-      .pipe(rename({extname: '.dart'}))
-      .pipe(gulp.dest(buildDir+'/spec'))
+  gulp.task('traceur/build', shell.task(
+    ['cd '+traceurDir+' && make bin/traceur.js bin/traceur-runtime.js']
+  ));
 
-  var copySrc = gulp.src(fixtureFiles).pipe(gulp.dest(buildDir+'/spec'));
-  return mergeStreams(transpileSrc, copySrc);
-});
+  // -- js2dart
 
-gulp.task('js2dart/test/run', shell.task([
-  'cd '+baseDir+' && dart --checked run_specs.dart'
-]));
+  var traceurJsOptions = {
+    annotations: true, // parse annotations
+    types: true, // parse types
+    script: false, // parse as a module
+    modules: 'register',
+    typeAssertions: false,
+    moduleName: true,
+    referrer: 'js2dart',
+    reload: true
+  };
 
-gulp.task('js2dart/test', ['js2dart/build'], function() {
-  return runSequence('js2dart/test/build', 'js2dart/test/run');
-});
+  var srcFiles = baseDir + '/src/**/*.js';
+  var specFiles = baseDir + '/spec/**/*.js';
+  var fixtureFiles = baseDir + '/spec/**/*.dart';
+  var buildDir = baseDir + '/build';
 
-gulp.task('js2dart/watch', function() {
-  watch([srcFiles, specFiles], function(_, done) {
-    runSequence('js2dart/test', done);
+  gulp.task('js2dart/clean', function() {
+    return gulp.src(buildDir, {read: false})
+        .pipe(clean());
   });
-});
+
+  gulp.task('js2dart/build', ['traceur/build'], function() {
+    return gulp
+      .src(srcFiles)
+      .pipe(traceur(traceurJsOptions))
+      .pipe(gulp.dest(buildDir+'/js2dart'))
+  });
+
+  gulp.task('js2dart/test/build', function() {
+    var transpileSrc =
+      gulp
+        .src(specFiles)
+        .pipe(js2dart())
+        .pipe(rename({extname: '.dart'}))
+        .pipe(gulp.dest(buildDir+'/spec'))
+
+    var copySrc = gulp.src(fixtureFiles).pipe(gulp.dest(buildDir+'/spec'));
+    return mergeStreams(transpileSrc, copySrc);
+  });
+
+  gulp.task('js2dart/test/run', shell.task([
+    'cd '+baseDir+' && dart --checked run_specs.dart'
+  ]));
+
+  gulp.task('js2dart/test', ['js2dart/build'], function() {
+    return runSequence('js2dart/test/build', 'js2dart/test/run');
+  });
+
+  // Needed so that gulp-js2dart and gulp-traceur pick up the newest changes
+  gulp.task('js2dart/refreshRequireCache', function(done) {
+    // TODO: This is not working right now / leads to additional errors
+    // - also needs to clear the module cache in System
+    // for (var prop in require.cache) {
+    //   if (/js2dart/.test(prop) || /traceur\//.test(prop)) {
+    //     delete require.cache[prop];
+    //   }
+    // }
+    done();
+  });
+
+  // Note: will also watch traceur for changes!
+  gulp.task('js2dart/watch', function() {
+    // TODO: also execute some of Traceur's tests (but not all as they take a while...)
+    return watch([traceurFiles, srcFiles, specFiles], function(_, done) {
+      return runSequence('js2dart/refreshRequireCache', 'js2dart/test', done);
+    });
+  });
+}
